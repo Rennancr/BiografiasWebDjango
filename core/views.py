3459ef5c9772ab24c8145a4390f20d97dd6ef3c5
Rennan Lima <rennancorreiaribeiro@gmail.com>
2023-05-10 00:51:16 -0300

    commit inicial

---
+++ core/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+from .models import *
+
+
+def index(request):
+    return render(request, 'core/index.html')
+
+
+def lista(request):
+    pesquisa = request.GET.get('pesquisa')
+    if pesquisa:
+        pessoas = Pessoa.objects.filter(nome__contains=pesquisa)
+    else:
+        pessoas = Pessoa.objects.all()
+    context = {
+        'pessoas': pessoas
+    }
+    return render(request, 'core/lista.html', context=context)
